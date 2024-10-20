dbd83f4ada3c73a08094dff9434d31acaf2816b6
DanielMelo0 <79672542+DanielMelo0@users.noreply.github.com>
2024-10-20 15:09:05 -0300

    First Commit

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth } from './helpers/dateFilter';
+
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* Área de Informações */}
+
+        {/* Área de Inserção */}
+
+        {/* Tabela de Itens */}
+      </C.Body>
+    </C.Container>
+  );
+};
+
+export default App;
