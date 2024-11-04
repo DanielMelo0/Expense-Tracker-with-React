5fff2367f410617c0e2d17c544f20451f1569e07
DanielMelo0 <79672542+DanielMelo0@users.noreply.github.com>
2024-11-04 11:54:06 -0300

    Update

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { Category } from './types/Category';
 import { items } from './data/items';
 import { categories } from './data/categories';
-import { getCurrentMonth } from './helpers/dateFilter';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
-  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
 
   return (
     <C.Container>
@@ -22,6 +27,7 @@ const App = () => {
         {/* Área de Inserção */}
 
         {/* Tabela de Itens */}
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
--- expense-tracker/src/data/items.ts
+++ expense-tracker/src/data/items.ts
@@ -2,25 +2,25 @@ import { Item } from '../types/Item';
 
 export const items: Item[] = [
   {
-    date: new Date(2024, 9, 15),
+    date: new Date(2024, 10, 15),
     category: 'food',
     title: 'McDonalds',
     value: 32.12,
   },
   {
-    date: new Date(2024, 9, 10),
+    date: new Date(2024, 10, 10),
     category: 'food',
     title: 'Burguer King',
     value: 30,
   },
   {
-    date: new Date(2024, 9, 9),
+    date: new Date(2024, 10, 9),
     category: 'rent',
     title: 'Aluguel Apt',
     value: 320,
   },
   {
-    date: new Date(2024, 9, 15),
+    date: new Date(2024, 10, 15),
     category: 'salary',
     title: 'Salário ACME',
     value: 4032.12,
